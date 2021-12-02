86c7091ec6a334059be70a4ec0e1820420ab4a79
rus733 <9282716617@mail.ru>
2021-12-02 21:26:58 +0300

    fix:in  script.js added wrap function gameBot

--- script.js
+++ script.js
@@ -2,32 +2,33 @@
 //игровой бот.
 //"Загадывание случайного числа от 1 до 100"
 
-const isNumber = function (num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
-};
-
-function inputNumber() {
+function gameBot() {
   let randomNum = parseInt(Math.random() * 100);
 
-  let userNum = prompt("Угадай число от 1 до 100");
+  const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  };
 
-  if (userNum === null) {
-    return alert("Игра закончена");
-  } else if (isNumber(userNum)) {
-    userNum = +userNum;
+  function inputNumber() {
+    let userNum = prompt("Угадай число от 1 до 100");
 
-    if (userNum > randomNum) {
-      alert("Загаданное число меньше, введите новый вариант числа");
-    } else if (userNum < randomNum) {
-      alert("Загаданное число больше, введите новый вариант числа");
-    } else if (userNum === randomNum) {
-      alert("Поздравляю, Вы угадали!!!");
-      return userNum;
+    if (userNum === null) {
+      return alert("Игра закончена");
+    } else if (isNumber(userNum)) {
+      userNum = +userNum;
+      if (userNum > randomNum) {
+        alert("Загаданное число меньше, введите новый вариант числа");
+      } else if (userNum < randomNum) {
+        alert("Загаданное число больше, введите новый вариант числа");
+      } else if (userNum === randomNum) {
+        alert("Поздравляю, Вы угадали!!!");
+        return userNum;
+      }
+    } else {
+      alert("Введите число!");
     }
-  } else {
-    alert("Введите число!");
+    return inputNumber();
   }
-  return inputNumber();
+  console.log(inputNumber());
 }
-
-console.log(inputNumber());
+gameBot();
